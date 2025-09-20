87b6b1f9a8a6d1e5841b34a7126daee5984d23ff
Bora Tugrul <boratugruls@gmail.com>
2025-09-20 18:36:36 +0300

    learned how to add base default styles and components

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,10 @@ function App() {
 
   return (
         <main className='w-full h-full flex flex-col items-center justify-center gap-4'>
-          <h1 className='text-purple text-xl underline'>Hey! I am learning fastt.</h1>
-          <div className='box bg-purple w-20 h-20'></div>
-          <button onClick={moveBox} className='border border-purple rounded-md bg-black text-purple py-2 px-4'> Click Me! </button>
+          <h1 className='text-xl underline'>Learning GSAP & Tailwind</h1>
+          <p className='text-wrap w-1/2 text-center mb-10'>Learned how to use custom components and layers. Now it's time to add some spice with diving deep into the animations.</p>
+          <div className='box bg-sea w-20 h-20 mb-10'></div>
+          <button onClick={moveBox} className='rounded-md bg-black py-2 px-4 opacity-60 hover:opacity-100 hover:outline-2 hover:outline-candy hover:outline-double'> Click Me! </button>
         </main>
   )
 }
